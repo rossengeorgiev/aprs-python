5e3903a9074b2874d647b686c685615b744087c3
Rossen Georgiev <zoorty@gmail.com>
2014-12-14 23:12:00 +0000

    applied GPLv2 license

--- aprs/__init__.py
+++ aprs/__init__.py
@@ -1,3 +1,19 @@
+# aprs - Python library for dealing with APRS
+# Copyright (C) 2013-2014 Rossen Georgiev
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 """
 APRS library in Python
@@ -5,8 +21,6 @@ APRS library in Python
 Currently the library provides facilities to:
     - parse APRS packets
     - Connect and listen to an aprs-is packet feed
-
-Copyright 2013-2014 (C), Rossen Georgiev
 """
 
 from datetime import date as _date
