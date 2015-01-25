rossengeorgiev/aprs-python

commit feb031d58736a4511689bc667879cb32644a6ade

fix incorrect parsing of timestamps, v0.6.34

aprslib/parse.py
@@ -316,11 +316,11 @@ def _parse_timestamp(body, packet_type=''):
             try:
                 # zulu hhmmss format
                 if form == 'h':
-                    timestamp = "%s%s%s%s" % (utc.year, utc.month, utc.day, ts)
+                    timestamp = "%d%02d02d%s" % (utc.year, utc.month, utc.day, ts)
                 # zulu ddhhmm format
                 # '/' local ddhhmm format
                 elif form in 'z/':
-                    timestamp = "%s%s%s%s" % (utc.year, utc.month, ts, utc.second)
+                    timestamp = "%d%02d%s%02d" % (utc.year, utc.month, ts, utc.second)
 
                 timestamp = utc.strptime(timestamp, "%Y%m%d%H%M%S")
                 timestamp = time.mktime(timestamp.timetuple())

aprslib/__init__.py
@@ -37,7 +37,7 @@ from datetime import date as _date
 __date__ = str(_date.today())
 del _date
 
-__version__ = "0.6.33"
+__version__ = "0.6.34"
 __author__ = "Rossen Georgiev"
 __all__ = ['IS', 'parse', 'passcode']
 

commit 3b912d6d68d0811024c064716c0a6124bfb5d7d3

fix typo in timestamp format
introduced in feb031d58736a4511689bc667879cb32644a6ade

aprslib/parse.py
@@ -337,7 +337,7 @@ def _parse_timestamp(body, packet_type=''):
             try:
                 # zulu hhmmss format
                 if form == 'h':
-                    timestamp = "%d%02d02d%s" % (utc.year, utc.month, utc.day, ts)
+                    timestamp = "%d%02d%02d%s" % (utc.year, utc.month, utc.day, ts)
                 # zulu ddhhmm format
                 # '/' local ddhhmm format
                 elif form in 'z/':