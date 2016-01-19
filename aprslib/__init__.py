1bdf8f21aebbd6c821fe68f8307c9a72ea84e5b3
Rossen Georgiev <rossen@rgp.io>
2016-01-19 22:26:57 +0000

    update CHANGES and verison number

--- aprslib/__init__.py
+++ aprslib/__init__.py
@@ -50,7 +50,7 @@ from datetime import date as _date
 __date__ = str(_date.today())
 del _date
 
-__version__ = "0.6.41"
+__version__ = "0.6.42"
 __author__ = "Rossen Georgiev"
 __all__ = ['IS', 'parse', 'passcode']
 
