f6a952a1091963dc7bbe4721e83b00f8df02d44e
Rossen Georgiev <rossen@rgp.io>
2017-01-28 23:24:20 +0200

    bump to v0.6.46

--- aprslib/__init__.py
+++ aprslib/__init__.py
@@ -41,7 +41,8 @@ from datetime import date as _date
 __date__ = str(_date.today())
 del _date
 
-__version__ = "0.6.45"
+__version__ = "0.6.46"
+version_info = (0, 6, 46)
 __author__ = "Rossen Georgiev"
 __all__ = ['IS', 'parse', 'passcode']
 
