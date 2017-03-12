8f0f72b5f8f31128c731ede2966730d5f980540b
Bryce Salmi <bryce@faradayrf.com>
2017-03-11 21:42:02 -0800

    Added APRS equations packet support
    TelemetryEquationsReport will generate a string of equation parameters to
    scale telemetry data to.

--- aprslib/packets/__init__.py
+++ aprslib/packets/__init__.py
@@ -2,3 +2,4 @@ from aprslib.packets.position import PositionReport
 from aprslib.packets.telemetry import TelemetryReport
 from aprslib.packets.telemetryunitlabels import TelemetryUnitLabelsReport
 from aprslib.packets.telemetryparameters import TelemetryParametersReport
+from aprslib.packets.telemetryequations import TelemetryEquationsReport
